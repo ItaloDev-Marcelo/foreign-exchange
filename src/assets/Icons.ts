b06778da3bcba048697831491a687ebb1476392b
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-08-09 14:18:02 -0300

    feat: creating Icons

---
+++ src/assets/Icons.ts
@@ -0,0 +1,25 @@
+import IconArrowRight from '../assets/images/icon-arrow-right.svg'
+import IconCheck from '../assets/images/icon-check.svg'
+import IconChevronDown from '../assets/images/icon-chevron-down.svg'
+import IconDeleteFilled from '../assets/images/icon-delete-filled.svg'
+import IconDelete from '../assets/images/icon-delete.svg'
+import IconExchangeVertical from '../assets/images/icon-exchange-vertical.svg'
+import IconExchange from '../assets/images/icon-exchange.svg'
+import IconSearch from '../assets/images/icon-search.svg'
+import IconStartFilled from '../assets/images/icon-star-filled.svg'
+import IconStart from '../assets/images/icon-star.svg'
+import IconLogo from '../assets/images/logo.svg'
+
+export {
+    IconArrowRight,
+    IconCheck,
+    IconChevronDown,
+    IconDeleteFilled,
+    IconDelete,
+    IconExchangeVertical,
+    IconExchange,
+    IconSearch,
+    IconStartFilled,
+    IconStart,
+    IconLogo 
+}
