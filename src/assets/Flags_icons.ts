3a9ae17f5e1863966e463ab87a46742a0a49df9d
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-08-09 14:17:39 -0300

    feat: creating Flags_icons

---
+++ src/assets/Flags_icons.ts
@@ -0,0 +1,124 @@
+import AE from '../assets/images/flags/ae.webp';
+import AR from '../assets/images/flags/ar.webp';
+import AU from '../assets/images/flags/au.webp';
+import BD from '../assets/images/flags/bd.webp';
+import BG from '../assets/images/flags/bg.webp';
+import BH from '../assets/images/flags/bh.webp';
+import BR from '../assets/images/flags/br.webp';
+import CA from '../assets/images/flags/ca.webp';
+import CH from '../assets/images/flags/ch.webp';
+import CL from '../assets/images/flags/cl.webp';
+import CN from '../assets/images/flags/cn.webp';
+import CO from '../assets/images/flags/co.webp';
+import CY from '../assets/images/flags/cy.webp';
+import CZ from '../assets/images/flags/cz.webp';
+import DK from '../assets/images/flags/dk.webp';
+import EG from '../assets/images/flags/eg.webp';
+import EU from '../assets/images/flags/eu.webp';
+import GB from '../assets/images/flags/gb.webp';
+import HK from '../assets/images/flags/hk.webp';
+import HM from '../assets/images/flags/hm.webp';
+import HN from '../assets/images/flags/hn.webp';
+import HR from '../assets/images/flags/hr.webp';
+import HT from '../assets/images/flags/ht.webp';
+import HU from '../assets/images/flags/hu.webp';
+import ID from '../assets/images/flags/id.webp';
+import IN from '../assets/images/flags/in.webp';
+import IS from '../assets/images/flags/is.webp';
+import JO from '../assets/images/flags/jo.webp';
+import JP from '../assets/images/flags/jp.webp';
+import KE from '../assets/images/flags/ke.webp';
+import KR from '../assets/images/flags/kr.webp';
+import KW from '../assets/images/flags/kw.webp';
+import LB from '../assets/images/flags/lb.webp';
+import LC from '../assets/images/flags/lc.webp';
+import LK from '../assets/images/flags/lk.webp';
+import MA from '../assets/images/flags/ma.webp';
+import MX from '../assets/images/flags/mx.webp';
+import MY from '../assets/images/flags/my.webp';
+import NG from '../assets/images/flags/ng.webp';
+import NO from '../assets/images/flags/no.webp';
+import NP from '../assets/images/flags/np.webp';
+import NZ from '../assets/images/flags/nz.webp';
+import OM from '../assets/images/flags/om.webp';
+import PE from '../assets/images/flags/pe.webp';
+import PH from '../assets/images/flags/ph.webp';
+import PK from '../assets/images/flags/pk.webp';
+import PL from '../assets/images/flags/pl.webp';
+import QA from '../assets/images/flags/qa.webp';
+import RO from '../assets/images/flags/ro.webp';
+import RU from '../assets/images/flags/ru.webp';
+import SA from '../assets/images/flags/sa.webp';
+import SE from '../assets/images/flags/se.webp';
+import SG from '../assets/images/flags/sg.webp';
+import TH from '../assets/images/flags/th.webp';
+import TR from '../assets/images/flags/tr.webp';
+import TW from '../assets/images/flags/tw.webp';
+import UA from '../assets/images/flags/ua.webp';
+import US from '../assets/images/flags/us.webp';
+import VN from '../assets/images/flags/vn.webp';
+import ZA from '../assets/images/flags/za.webp';
+
+
+export const ContriesFlags: Record<string, string> = {
+ 'AE':  AE,  
+ 'AR':  AR,  
+ 'AU':  AU,  
+ 'BD':  BD,
+ 'BG':  BG,
+ 'BH':  BH,
+ 'BR':  BR,
+ 'CA':  CA,
+ 'CH':  CH,
+ 'CL':  CL,
+ 'CN':  CN,
+ 'CO':  CO,
+ 'CY':  CY,
+ 'CZ':  CZ,
+ 'DK':  DK,
+ 'EG':  EG,
+ 'EU':  EU,
+ 'GB':  GB,
+ 'HK':  HK,
+ 'HM':  HM,
+ 'HN':  HN,
+ 'HR':  HR,
+ 'HT':  HT,
+ 'HU':  HU,
+ 'ID':  ID,
+ 'IN':  IN,
+ 'IS':  IS,
+ 'JO':  JO,
+ 'JP':  JP,
+ 'KE':  KE,
+ 'KR':  KR,
+ 'KW':  KW,
+ 'LB':  LB,  
+ 'LC':  LC,  
+ 'LK':  LK,  
+ 'MA':  MA,  
+ 'MX':  MX,  
+ 'MY':  MY,  
+ 'NG':  NG,  
+ 'NO':  NO,  
+ 'NP':  NP,  
+ 'NZ':  NZ,  
+ 'OM':  OM,  
+ 'PE':  PE,  
+ 'PH':  PH,  
+ 'PK':  PK,  
+ 'PL':  PL,  
+ 'QA':  QA,  
+ 'RO':  RO,  
+ 'RU':  RU,  
+ 'SA':  SA,  
+ 'SE':  SE,  
+ 'SG':  SG,  
+ 'TH':  TH,  
+ 'TR':  TR,  
+ 'TW':  TW,  
+ 'UA':  UA,  
+ 'US':  US,  
+ 'VN':  VN,  
+ 'ZA':  ZA
+}
